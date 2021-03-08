337152eca2843e51622e376699d04403e8ce9934
Dark <isaaklux@hotmail.com>
2021-03-08 14:20:42 +0100

    walls

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -21,10 +21,18 @@ public class Ball : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        float maxDistance = other.transform.localScale.x * 1 * 0.5f + transform.localScale.x * 1 * 0.5f;
-        float dist = transform.position.x - other.transform.position.x;
-        float nDist = dist / maxDistance;
+        if (other.CompareTag("Paddle"))
+        {
+            float maxDistance = other.transform.localScale.x * 1 * 0.5f + transform.localScale.x * 1 * 0.5f;
+            float dist = transform.position.x - other.transform.position.x;
+            float nDist = dist / maxDistance;
         
-        velocity = new Vector3(nDist * maxX, velocity.y, -velocity.z);
+            velocity = new Vector3(nDist * maxX, velocity.y, -velocity.z);
+        }
+        else if (other.CompareTag("Wall"))
+        {
+            velocity = new Vector3(-velocity.x, velocity.y, velocity.z);
+        }
+        GetComponent<AudioSource>().Play();
     }
 }
